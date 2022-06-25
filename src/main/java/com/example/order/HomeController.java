0b604ad62786b551d5004433ad37c91b71413ba1
MikeQin <mike.qin@gmail.com>
2022-06-24 23:50:18 -0500

    Added MVC

---
+++ src/main/java/com/example/order/HomeController.java
@@ -0,0 +1,24 @@
+package com.example.order;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	@ResponseStatus(HttpStatus.OK)
+	public String home(Model user) {
+		return "home";
+	}
+	
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+}
